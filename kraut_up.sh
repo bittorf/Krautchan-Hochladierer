7b19419e3ab458539a4f89c8e9a580310a606cf4
blargh <blargh.macfag@googlemail.com>
2011-05-30 18:56:43 +0200

    optionen für säge, combo und hilfe

--- kraut_up.sh
+++ kraut_up.sh
@@ -1,72 +1,101 @@
 #!/usr/bin/env bash
 
-CURL=$(which curl)
-if [ -z "${CURL}" ]; then
+curl=$(which curl)
+if [ -z "${curl}" ]; then
 	echo "Dieses Skript benötigt cURL. Vergewissere dich dass es installiert ist und im Suchpfad liegt."
 	exit 1
 fi
 
-UA="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1"
-PWD=""
-POST_URL="http://krautchan.net/post"
-DELETE_URL="http://krautchan.net/delete"
-BIFS=${IFS}
-NAME_ALLOWED=""
-COUNT="0"
-ARR_KIND=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd" -o -iname "*.mp3" -o -iname "*.ogg" -o -iname "*.rar" -o -iname "*.zip" -o -iname "*.torrent" -o -iname "*.swf")
+ua="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1"
+post_url="http://krautchan.net/post"
+#delete_url="http://krautchan.net/delete"
+#pwd=""
+bifs=${IFS}
+name_allowed=""
+optional=""
+combo=0
+files_allowed=""
+count=0
+arr_kind=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd" -o -iname "*.mp3" -o -iname "*.ogg" -o -iname "*.rar" -o -iname "*.zip" -o -iname "*.torrent" -o -iname "*.swf")
+
+while getopts hsoc: opt; do
+	case "${opt}" in
+		h)
+		echo "
+kraut_up.sh [-soh] [-c 1-4]
+
+Wiezu:
+ -s	Säge!
+ -c n	Begrenzt die erlaubten Dateien pro Pfostierung auf n. Nützlich für Combos.
+	Berücksichtige, dass z.B. 11.jpg vor 2.jpg einsortiert wird!
+ -o	Optionale Abfragen (Name, Betreff und Kommentar) werden aktiviert.
+ -h	Diese Hilfe."
+		exit
+		;;
+		s)
+		sage=1
+		;;
+		o)
+		optional=1
+		;;
+		c)
+		combo=${OPTARG}
+		;;
+	esac
+done
 
 choose() {
-echo -ne "Wähle ein Brett aus>
+echo -ne "Wähle ein Brett aus
  (b,int,vip,a,c,d,e,f,fb,fit,jp,k,l,li,m,p,ph,sp,t,tv
   v,w,we,wp,x,n,rfk,z,zp,h,s,kc): "
-read -e BOARD
+read -e board
 
-case "${BOARD}" in #NEGER, BITTE!
+case "${board}" in #NEGER, BITTE!
 	b|int|vip)
-	NAME_ALLOWED=no
-	FILES_ALLOWED=4
-	MAX_FILE_SIZE=10M
+	name_allowed=no
+	files_allowed=4
+	max_file_size=10M
 	;;
 	a|jp)
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=9M
+	files_allowed=3
+	max_file_size=9M
 	;;
 	k)
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=10M
-	#MAX_POST_SIZE=15
+	files_allowed=3
+	max_file_size=10M
+	#max_post_size=15
 	;;
 	l|m)
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=20M
-	#MAX_POST_SIZE=40
+	files_allowed=3
+	max_file_size=20M
+	#max_post_size=40
 	;;
 	c|fb|p|tv|v|we)
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=6M
+	files_allowed=3
+	max_file_size=6M
 	;;
 	wp)
-	ARR_KIND=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd")
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=6M
+	arr_kind=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd")
+	files_allowed=3
+	max_file_size=6M
 	;;
 	rfk)
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=5M
+	files_allowed=3
+	max_file_size=5M
 	;;
 	z|zp|s)
-	ARR_KIND=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd")
-	FILES_ALLOWED=4
-	MAX_FILE_SIZE=6M
+	arr_kind=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd")
+	files_allowed=4
+	max_file_size=6M
 	;;
 	h)
-	ARR_KIND=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd")
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=3M
+	arr_kind=(-iname "*.gif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.psd")
+	files_allowed=3
+	max_file_size=3M
 	;;
 	d|e|f|fit|li|ph|sp|t|w|x|n|kc)
-	FILES_ALLOWED=3
-	MAX_FILE_SIZE=3M
+	files_allowed=3
+	max_file_size=3M
 	;;
 	*)
 	echo -e "\nDepp.\n"
@@ -78,76 +107,89 @@ esac
 clear
 choose
 
-echo -ne "\nFaden-ID>
+if [ "${combo}" -gt "${files_allowed}" ]; then
+	echo -ne "\nEine ${combo}er-Combo ist nicht möglich da auf /${board}/ nur ${files_allowed} Dateien pro Pfostierung erlaubt sind.\n"
+	exit
+elif [ "${combo}" -lt "${files_allowed}" ]; then
+	files_allowed=${combo}
+fi
+
+echo -ne "\nFaden-ID
  (z.B. 3025905 - leer lassen um einen neuen Faden zu erstellen): "
-read -e ID
+read -e id
 
 echo -ne "\nVerzeichniss(e) auswählen. Leerzeichen müssen escaped werden.
  (z.B.: /Users/bernd/penisbilder /home/bernadette/als\ ob):\n"
-read -a ARR_DIR
-
-IFS='
-'
-for DIR in ${ARR_DIR[@]}; do
-	for FILES in $(find ${DIR} -type f -size -${MAX_FILE_SIZE} \( ${ARR_KIND[@]} \) ); do
-		ARR_FILES+=("${FILES}")
-	done
-done
-IFS=${BIFS}
-#echo ${ARR_FILES[@]}
-ARR_FILES+=(END)
-
-if [ -z ${NAME_ALLOWED} ]; then
-	echo -ne "\nName>
- (Optional): "
-	read -e NAME
-fi
-
-echo -ne "\nBetreff>
- (Optional, wird nur ein mal pfostiert): "
-read -e ISUB
-
-echo -ne "\nKommentar>
- (Notwendig für neue Fäden, wird nur ein mal pfostiert): "
-read -e ICOM
+read -a arr_dir
 
 echo
 
-for FILE in "${ARR_FILES[@]}"; do
-	let "COUNT += 1"
-	if [ "${FILE}" != "END" ]; then
-		if [ ${COUNT} -eq "1" ]; then
-			ARR_CURL+=(-F file_0=@${FILE})
+if [ -n "${optional}" ]; then
+	if [ -z "${name_allowed}" ]; then
+		echo -ne "Name: "
+		read -e name
+	fi
+	echo -ne "\nBetreff
+ (Wird nur ein mal pfostiert): "
+	read -e isub
+	echo -ne "\nKommentar
+ (Wird nur ein mal pfostiert): "
+	read -e icom
+elif [ -z "${id}" ] && [ -z "${icom}" ]; then
+	echo -ne "Kommentar
+ (Ist nötig weil ein neuer Faden erstellt wird. Wird nur ein mal pfostiert): "
+	read -e icom
+fi
+
+IFS='
+'
+for dir in "${arr_dir[@]}"; do
+	for files in $(find ${dir} -type f -size -${max_file_size} \( ${arr_kind[@]} \) ); do
+		arr_files+=("${files}")
+	done
+done
+IFS=${bifs}
+
+arr_files+=(END)
+
+for file in "${arr_files[@]}"; do
+	let "count += 1"
+	if [ "${file}" != "END" ]; then
+		if [ "${files_allowed}" -eq "1" ]; then
+			arr_curl+=(-F file_0=@${file})
+		elif [ "${count}" -eq "1" ]; then
+			arr_curl+=(-F file_0=@${file})
 			continue
-		elif [ ${COUNT} -eq "2" ]; then
-			ARR_CURL+=(-F file_1=@${FILE})
+		elif [ "${files_allowed}" -eq "2" ]; then
+			arr_curl+=(-F file_1=@${file})
+		elif [ "${count}" -eq "2" ]; then
+			arr_curl+=(-F file_1=@${file})
 			continue
-		elif [ ${FILES_ALLOWED} -eq "3" ]; then
-			ARR_CURL+=(-F file_2=@${FILE})
-		elif [ ${COUNT} -eq "3" ]; then
-			ARR_CURL+=(-F file_2=@${FILE})
+		elif [ "${files_allowed}" -eq "3" ]; then
+			arr_curl+=(-F file_2=@${file})
+		elif [ "${count}" -eq "3" ]; then
+			arr_curl+=(-F file_2=@${file})
 			continue
-		elif [ ${COUNT} -eq "4" ]; then
-			ARR_CURL+=(-F file_3=@${FILE})
+		elif [ "${count}" -eq "4" ]; then
+			arr_curl+=(-F file_3=@${file})
 		fi
-	# verhindert curl-fehler im falle von ${FILES_ALLOWED}|${ARR_FILES[@]}
-	elif [ "${FILE}" = "END" ] && [ ${COUNT} -eq "1" ]; then
+	# verhindert curl-fehler im falle von ${files_allowed}|${arr_files[@]}
+	elif [ "${file}" = "END" ] && [ "${count}" -eq "1" ]; then
 		exit
 	fi
 
-	OUTPUT=$(${CURL} -# -A "${UA}" -F "board=${BOARD}" -F "parent=${ID}" -F "forward=thread" -F "internal_n=${NAME}" -F "internal_s=${ISUB}" -F "internal_t=${ICOM}" "${ARR_CURL[@]}" "${POST_URL}")
+	output=$(${curl} -# -A "${ua}" -F "sage=${sage}" -F "board=${board}" -F "parent=${id}" -F "forward=thread" -F "internal_n=${name}" -F "internal_s=${isub}" -F "internal_t=${icom}" "${arr_curl[@]}" "${post_url}")
 
-	if [ -z ${ID} ]; then
-		ID=$(echo ${OUTPUT} | egrep -o '\-[0-9]+\.' | egrep -o '[0-9]+')
-		echo "Neuen Faden erstellt: http://krautchan.net/${BOARD}/thread-${ID}.html"
+	if [ -z "${id}" ]; then
+		id=$(echo ${output} | egrep -o '\-[0-9]+\.' | egrep -o '[0-9]+')
+		echo "Neuen Faden erstellt: http://krautchan.net/${board}/thread-${id}.html"
 	fi
 	
 	# debug
-	#echo -ne "\n\n######################\n\n${OUTPUT}" >> ${HOME}/Desktop/debug.txt
+	#echo -ne "\n\n######################\n\n${output}" >> ${HOME}/Desktop/debug.txt
 	
-	COUNT="0"
-	unset ARR_CURL
-	ISUB=""
-	ICOM=""
-	
+	count=0
+	unset arr_curl
+	isub=""
+	icom=""
 done
